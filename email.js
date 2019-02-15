581819d69ffff4ca0afe4ef53547a331ba76e63f
Conor O'Flanagan <oflanac52@gmail.com>
2019-02-15 23:46:50 +0000

    style the email

--- email.js
+++ email.js
@@ -1,32 +1,55 @@
 const nodemailer = require('nodemailer')
 
-const emailProps = async (email, res) => {
+const emailProps = async (email = 'test', res) => {
   let account = await nodemailer.createTestAccount()
 
   let transporter = nodemailer.createTransport({
-    host: 'smtp.sendgrid.net',
+    host: 'smtp.sendgrid.net', // <-- real smtp server
     port: 465,
     secure: true,
     auth: {
       user: 'apikey',
       pass: process.env.MAILKEY
     }
+    // host: 'smtp.ethereal.email', // <-- test smtp server
+    // port: 587,
+    // secure: false,
+    // auth: {
+    //   user: account.user,
+    //   pass: account.pass
+    // }
   })
 
-  // setup email data with unicode symbols
+  const html = `
+  <h3>🏠🤖 Propito property list 🤖🏠</h3>
+  <div>${res
+    .map(
+      entry =>
+        `<ul style='background-color: #cdfbff;
+        border-radius: 10px;
+        padding: 20px;
+        list-style: none;'>${Object.keys(entry)
+    .map((prop, i, arr) => `<li>${JSON.stringify(entry[prop])}</li>`)
+    .join('')}</ul>`
+    )
+    .join()}</div>`
+  console.log(html)
+
   let mailOptions = {
     from: email + '@gmail.com', // sender address
     to: email + '@gmail.com', // list of receivers
-    subject: 'properties list', // Subject line
+    subject: 'Propito Daily Properties List', // Subject line
     text: JSON.stringify(res), // plain text body
-    html: `<b>${JSON.stringify(res, undefined, 2)}</b>` // html body
+    html // html body
   }
 
   let info = await transporter.sendMail(mailOptions)
 
   console.log('Message sent: %s', info.messageId)
   // Preview only available when sending through an Ethereal account
-  // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
 }
+
+emailProps()
 
 module.exports = emailProps
